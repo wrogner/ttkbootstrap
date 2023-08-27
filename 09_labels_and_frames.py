4ddaa32b945f5643f08e75386f6b738d69b049a7
wolf <war@rsb.at>
2023-08-27 21:04:24 +0200

    labels and frames scaffold

---
+++ 09_labels_and_frames.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" 09_labels_and_frames.py
+
+Create frames and some things about labels
+
+:author:	wolf
+:created:	2023.08.27
+"""
+
+
+import tkinter as tk
+import ttkbootstrap as tbs
+from ttkbootstrap.constants import *
+
+app = tbs.Window(themename="cosmo")
+app.title("TTK Bootstrap!")
+app.iconbitmap("img/favicon.ico")
+app.geometry("500x350")
+
+
+app.mainloop()
