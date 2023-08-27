54c35feef9a7fe011746964997350d1ba46d740a
wolf <war@rsb.at>
2023-08-27 21:22:35 +0200

    final label and frame

--- 09_labels_and_frames.py
+++ 09_labels_and_frames.py
@@ -31,7 +31,7 @@ entry_1.pack(pady=20, padx=20)
 button_1 = tbs.Button(frame_1, text="CLICK ME!", bootstyle="outline", command=button_clicked)
 button_1.pack(pady=20, padx=20)
 
-label_1 = tbs.Label(app, text="Hello there!", justify="center", font=(".AppleSystemUIFont", 18), bootstyle="inverse dark")
-label_1.pack(pady=20, ipady=10, ipadx=30)
+label_1 = tbs.Label(app, text="Hello there!", font=(".AppleSystemUIFont", 18), bootstyle="dark")
+label_1.pack(pady=20)
 
 app.mainloop()
