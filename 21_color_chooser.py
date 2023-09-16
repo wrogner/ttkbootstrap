34c5748ab8c9cf1cec5b41acc78ff89bfd980fd7
wolf <war@rsb.at>
2023-09-16 17:22:53 +0200

    color chooser scaffold

---
+++ 21_color_chooser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+""" 21_color_chooser.py
+
+Color chooser
+
+:author:	wolf
+:created:	2023.09.16
+"""
+
+
+import tkinter as tk
+import ttkbootstrap as tbs
+from ttkbootstrap.constants import *
+
+
+app = tbs.Window(themename="cosmo")
+app.title("TTK Bootstrap!")
+app.iconbitmap("img/favicon.ico")
+app.geometry("500x350")
+
+
+
+app.mainloop()
